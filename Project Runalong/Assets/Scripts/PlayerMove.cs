f3a0a3b2618f7a2df93babd5df246b83b694fe36
lyrari <lyrariley1@gmail.com>
2022-04-22 22:19:39 -0700

    Initial player controller complete

--- Project Runalong/Assets/Scripts/PlayerMove.cs
+++ Project Runalong/Assets/Scripts/PlayerMove.cs
@@ -10,34 +10,87 @@ public class PlayerMove : MonoBehaviour
     public LayerMask GroundMask;
 
     // Player data
+    [Space]
     private Rigidbody2D rb;
-    private BoxCollider2D boxCollider;
+    private SpriteRenderer playerSprite;
+    private float defaultGravity;
+    public BoxCollider2D MainCollider;
+    public BoxCollider2D CrouchCollider;
 
     // Tracking player state
     [SerializeField]
     private bool isGrounded;
     private bool jump;
+    [SerializeField]
+    private bool sliding;
+    private bool startFastFall;
+    [SerializeField]
+    private bool fastfalling;
+    private bool startGlide;
+    [SerializeField]
+    private bool gliding;
+
+    private float currentFloatTime;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        playerSprite = GetComponentInChildren<SpriteRenderer>();
+        defaultGravity = rb.gravityScale;
+        stopSliding();
     }
 
     // Update is called once per frame
+    // TODO: Generalize controls if it needs to be used on mobile too
     void Update()
     {
         // Raycast down to see if player is grounded, set variable
-        RaycastHit2D raycast = Physics2D.Raycast(boxCollider.bounds.center, Vector2.down, boxCollider.bounds.extents.y + 0.1f, GroundMask);
+        BoxCollider2D activeCollider = sliding ? CrouchCollider : MainCollider;
+        RaycastHit2D raycast = Physics2D.Raycast(activeCollider.bounds.center, Vector2.down, activeCollider.bounds.extents.y + 0.1f, GroundMask);
         isGrounded = raycast.collider != null;
 
+        // Cancel fastfalling if player lands
+        if (isGrounded && fastfalling)
+        {
+            fastfalling = false;
+        }
+
         // Check to see if player pressed a jump key
-        // TODO: Generalize this if it needs to be used on mobile too
-        // TODO: Player glide, Player slide, Player fastfall (?)
+        // Allow if: Pressed jump, grounded
         if (Input.GetMouseButtonDown(0) && isGrounded)
         {
             jump = true;
+            stopSliding();
+        }
+
+
+        // Check to see if player wants to glide
+        // Allow if: Holding jump, not fastfalling grounded or gliding, and is descending
+        if (Input.GetMouseButton(0) && !fastfalling && !isGrounded && !gliding && rb.velocity.y <= 0)
+        {
+            startGlide = true;
+        } else if (gliding && !Input.GetMouseButton(0))
+        {
+            stopGliding();
+        }
+
+        // Check to see if player wants to slide, or stop sliding
+        // Allow if: Holding slide, not already sliding or jumping, is grounded
+        if (Input.GetMouseButton(1) && isGrounded && !jump && !sliding)
+        {
+            startSliding();
+
+        } else if (!Input.GetMouseButton(1) && sliding)
+        {
+            stopSliding();
+        }
+
+        // Check to see if player wants to fastfall
+        // Allow if: Pressed slide, not grounded, not fastfalling already
+        if (Input.GetMouseButtonDown(1) && !isGrounded && !fastfalling)
+        {
+            startFastFall = true;
         }
     }
 
@@ -52,5 +105,48 @@ public class PlayerMove : MonoBehaviour
             jump = false;
             rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
+
+        if (startFastFall)
+        {
+            startFastFall = false;
+            fastfalling = true;
+            stopGliding(); // Stops gliding before fastfall
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.down * JumpForce, ForceMode2D.Impulse);
+        }
+
+        if (startGlide)
+        {
+            startGliding();
+        }
+    }
+
+    private void startSliding()
+    {
+        sliding = true;
+        MainCollider.enabled = false;
+        CrouchCollider.enabled = true;
+        playerSprite.transform.localScale = new Vector3(1, 0.5f);
+    }
+    private void stopSliding()
+    {
+        sliding = false;
+        MainCollider.enabled = true;
+        CrouchCollider.enabled = false;
+        playerSprite.transform.localScale = new Vector3(1, 1);
+    }
+
+    private void startGliding()
+    {
+        startGlide = false;
+        gliding = true;
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.gravityScale = 0.5f;
+    }
+
+    private void stopGliding()
+    {
+        gliding = false;
+        rb.gravityScale = defaultGravity;
     }
 }
